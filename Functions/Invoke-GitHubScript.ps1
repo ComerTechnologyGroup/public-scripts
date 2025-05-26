37a09cd6eacb6ce49f8b1a59cdd02d2ca4494ff4
Quinntin Comer <qcomer@comertechnology.com>
2025-05-25 21:24:06 -0700

    Changes

---
+++ Functions/Invoke-GitHubScript.ps1
@@ -0,0 +1,36 @@
+function Invoke-GithubScript {
+    <#
+    .SYNOPSIS
+    Calls Github Script from RMM Script
+    #>
+    param (
+        # Sets the URL of the script to download the script
+        [Parameter(mandatory = $false, position = 1, ValueFromPipeline = $false)]
+        [string]
+        $ScriptURL,
+        [Parameter(mandatory = $false, position = 2, ValueFromPipeline = $false)]
+        [string]
+        $Function
+    )
+    try {
+        Add-Type -AssemblyName System.Web
+        foreach ($paramName in $MyInvocation.MyCommand.Parameters.keys) {
+            $paramValue = Get-Variable -Scope Local -Name $paramName -ValueOnly -ErrorAction SilentlyContinue
+            if ($paramName -in ('ScriptURL', 'Function') -and $null -eq $paramValue) {
+                Write-Output "ERROR: Parameter $paramName is not set"
+                exit 1
+            }
+        }
+        $url                                        = [System.Web.HttpUtility]::UrlDecode($ScriptURL)
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        $wc                                         = New-Object System.Net.WebClient
+        $wc.Headers.Add('Accept', 'application/vnd.github.v3.raw')
+        $wc.Headers.Add('X-GitHub-Api-Version', '2022-11-28')
+        $wc.Headers.Add('Content-Type', 'application/json;charset=UTF-8')
+        $wc.DownloadString("$URL") | Invoke-Expression
+        $Function
+    } catch {
+        Write-Output "ERROR: $($_.Exception.message)"
+    }
+}
+Invoke-GithubScript -ScriptURL '@ScriptURL@' -Function '@Function@'
